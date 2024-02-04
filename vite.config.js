9a7aeffa6633ea3c81937ce66aab897961c97b44
Mukivarman <133698074+Mukivarman@users.noreply.github.com>
2024-02-05 00:14:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
-    proxy:{
-      "/api/":{
-        target:"https://supportdesk-hm1g.onrender.com/",
-        changeOrigin:true
-      }
-    }
-  },
+
   plugins: [react()],
   
 })
