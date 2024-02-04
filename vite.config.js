ac2f50b2a7c90f4e4c61d2c4da181694231cc157
Mukivarman <133698074+Mukivarman@users.noreply.github.com>
2024-02-04 21:32:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api/":{
-        target:"https://ill-gray-fly-tux.cyclic.app/",
+        target:"https://supportdesk-hm1g.onrender.com",
         changeOrigin:true
       }
     }
